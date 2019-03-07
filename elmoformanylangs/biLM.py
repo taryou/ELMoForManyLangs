7dcda491c73a79c479e7cade6106e42963f0202c
Hiroaki Ozaki <hiroaki.ozaki.yu@hitachi.com>
2019-03-07 13:07:33 +0900

    support data parallel

--- elmoformanylangs/biLM.py
+++ elmoformanylangs/biLM.py
@@ -295,7 +295,7 @@ def eval_model(model, valid):
   valid_w, valid_c, valid_lens, valid_masks = valid
   for w, c, lens, masks in zip(valid_w, valid_c, valid_lens, valid_masks):
     loss_forward, loss_backward = model.forward(w, c, masks)
-    total_loss += loss_forward.data[0]
+    total_loss += loss_forward.data.item()
     n_tags = sum(lens)
     total_tag += n_tags
   model.train()
@@ -341,7 +341,7 @@ def train_model(epoch, opt, model, optimizer,
     loss_forward, loss_backward = model.forward(w, c, masks)
 
     loss = (loss_forward + loss_backward) / 2.0
-    total_loss += loss_forward.data[0]
+    total_loss += loss_forward.data.item()
     n_tags = sum(lens)
     total_tag += n_tags
     loss.backward()
