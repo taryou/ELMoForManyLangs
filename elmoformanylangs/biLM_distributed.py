68b0d2830fcac9f213d4f92cc033b57ec26461c7
Hiroaki Ozaki <hiroaki.ozaki.yu@hitachi.com>
2019-03-14 20:46:02 +0900

    update NCCL distribution

--- elmoformanylangs/biLM_distributed.py
+++ elmoformanylangs/biLM_distributed.py
@@ -152,6 +152,7 @@ def average_gradients(model):
     size = float(dist.get_world_size())
     update = 0
     for param in model.parameters():
+        print(param, dir(param))
         dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM)
         update += torch.sum(torch.abs(param.grad.data))
         param.grad.data /= size
